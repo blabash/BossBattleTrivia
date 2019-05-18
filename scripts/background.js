e6c7cc0585a4864379e18e5180d8c2921ede0b4c
brett <brett.labash@gmail.com>
2019-05-17 18:36:55 -0700

    Add some question logic, still broken

--- scripts/background.js
+++ scripts/background.js
@@ -2,9 +2,9 @@
 
 
 const colorArray = [ //flame color palette
-    "rgb(255, 0, 0, .08)",
-    "rgb(255, 90, 0, .08)",
-    "rgb(255, 154, 0, .08)"
+    "rgb(255, 0, 0, .11)",
+    "rgb(255, 90, 0, .11)",
+    "rgb(255, 154, 0, .11)"
 ];
 
 //interactivity for when mouse is near circle
@@ -89,3 +89,36 @@ function Background(ctx) {
 }
 
 module.exports = Background;
+
+//canvas example code
+//rectangle/square
+// ctx.fillStyle = "rgba(255, 0, 0, .5";
+// ctx.fillRect(100, 100, 100, 100); //rectangle takes the fillstyle that precedes it
+// ctx.fillStyle = "rgba(0, 255, 0, .5";
+// ctx.fillRect(200, 200, 100, 100);
+// ctx.fillStyle = "rgba(0, 0, 255, .5";
+// ctx.fillRect(300, 300, 100, 100);
+
+// //line
+// ctx.beginPath();
+// ctx.moveTo(50, 300);
+// ctx.lineTo(300, 100);
+// ctx.lineTo(400, 300);
+// ctx.strokeStyle = "#fa34a3"
+// ctx.stroke();
+
+//arc/circle
+// ctx.beginPath()
+// ctx.arc(300, 300, 30, 0, Math.PI * 2, false);
+// ctx.strokeStyle = "blue";
+// ctx.stroke();
+// const COLORS = ["blue", "red", "green"];
+
+// for (let i = 0; i < 100; i++) {
+//     let x = Math.random() * window.innerWidth;
+//     let y = Math.random() * window.innerHeight;
+//     ctx.beginPath();
+//     ctx.arc(x, y, 30, 0, Math.PI * 2, false);
+//     ctx.strokeStyle = COLORS[Math.floor(Math.random() * COLORS.length)];
+//     ctx.stroke();
+// }
--- src/index.js
+++ src/index.js
@@ -16,35 +16,139 @@ window.addEventListener('resize', () => {
     canvas.height = window.innerHeight;
 });
 
+var triviaQuestions = [{
+    question: "In what year was the Molten Core raid released?",
+    answerList: ["2002", "2003", "2005", "2004"],
+    answer: 3
+    }, 
+    { 
+    question: "What was the name of Ragnaros' majordomo?",
+    answerList: ["Moroes", "Bill Gates", "Executus", "Van Cleef"],
+    answer: 2
+}];
 
-//rectangle/square
-// ctx.fillStyle = "rgba(255, 0, 0, .5";
-// ctx.fillRect(100, 100, 100, 100); //rectangle takes the fillstyle that precedes it
-// ctx.fillStyle = "rgba(0, 255, 0, .5";
-// ctx.fillRect(200, 200, 100, 100);
-// ctx.fillStyle = "rgba(0, 0, 255, .5";
-// ctx.fillRect(300, 300, 100, 100);
+let currentQuestion; 
+let correctAnswer; 
+let incorrectAnswer;
+let unanswered;
+let seconds; 
+let time; 
+let answered; 
+let userSelect;
 
-// //line
-// ctx.beginPath();
-// ctx.moveTo(50, 300);
-// ctx.lineTo(300, 100);
-// ctx.lineTo(400, 300);
-// ctx.strokeStyle = "#fa34a3"
-// ctx.stroke();
+const messages = {
+    correct: "Yes, that's right",
+    incorrect: "No, that's not it.",
+    endTime: "Out of time!",
+    finished: "Let's see how you did."
+}
 
-//arc/circle
-// ctx.beginPath()
-// ctx.arc(300, 300, 30, 0, Math.PI * 2, false);
-// ctx.strokeStyle = "blue";
-// ctx.stroke();
-// const COLORS = ["blue", "red", "green"];
+document.getElementById('startBtn').addEventListener('click', () => {
+    startBtn.style.display = 'none';
+    newGame();
+});
 
-// for (let i = 0; i < 100; i++) {
-//     let x = Math.random() * window.innerWidth;
-//     let y = Math.random() * window.innerHeight;
-//     ctx.beginPath();
-//     ctx.arc(x, y, 30, 0, Math.PI * 2, false);
-//     ctx.strokeStyle = COLORS[Math.floor(Math.random() * COLORS.length)];
-//     ctx.stroke();
-// }
+document.getElementById('startOverBtn').addEventListener('click', () => {
+    startOverBtn.style.display = 'none';
+    newGame();
+});
+
+function newGame() {
+    document.getElementById('finalMessage').innerHTML = '';
+    document.getElementById('correctAnswers').innerHTML = '';
+    document.getElementById('incorrectAnswers').innerHTML = '';
+    document.getElementById('unanswered').innerHTML = '';
+    currentQuestion = 0;
+    correctAnswer = 0;
+    incorrectAnswer = 0;
+    unanswered = 0;
+    newQuestion();
+}
+
+function newQuestion() {
+    document.getElementById('message').innerHTML = '';
+    document.getElementById('correctedAnswer').innerHTML = '';
+    answered = true;
+
+    //sets up new questions & answerList
+    document.getElementById('currentQuestion').innerHTML = 'Question #' + (currentQuestion + 1) + '/' + triviaQuestions.length;
+    document.getElementsByClassName('question').innerHTML = '<h2>' + triviaQuestions[currentQuestion].question + '</h2>';
+    for (let i = 0; i < 4; i++) {
+        let choices = document.createElement('div');
+        choices.textContent = triviaQuestions[currentQuestion].answerList[i];
+        choices.setAttribute('data-index', i);
+        choices.setAttribute('id', 'thisChoice');
+        // choices.classList.add('thisChoice');
+        document.getElementById('answerList').appendChild(choices)
+    }
+    countdown();
+    //clicking an answer will pause the time and setup answerPage
+    let thisChoice = document.getElementById('thisChoice');
+    // let thisChoice = document.getElementsByClassName('thisChoice');
+    thisChoice.addEventListener('click', () => {
+        userSelect = thisChoice.getAttribute('index');
+        clearInterval(time);
+        answerPage();
+    });
+}
+
+function countdown() {
+    seconds = 15;
+    document.getElementById('timeLeft').innerHTML = '<h3>Time Remaining: ' + seconds + '</h3>';
+    answered = true;
+    //sets timer to go down
+    time = setInterval(showCountdown, 1000);
+}
+
+function showCountdown() {
+    seconds--;
+    document.getElementById('timeLeft').innerHTML = '<h3>Time Remaining: ' + seconds + '</h3>';
+    if (seconds < 1) {
+        clearInterval(time);
+        answered = false;
+        answerPage();
+    }
+}
+
+function answerPage() {
+    document.getElementById('currentQuestion').innerHTML = '';
+    document.getElementsByClassName('thisChoice').innerHTML = '';
+    document.getElementsByClassName('question').innerHTML = '';
+
+    let rightAnswerText = triviaQuestions[currentQuestion].answerList[triviaQuestions[currentQuestion].answer];
+    let rightAnswerIndex = triviaQuestions[currentQuestion].answer;
+    //checks to see correct, incorrect, or unanswered
+    if ((userSelect == rightAnswerIndex) && (answered == true)) {
+        correctAnswer++;
+        document.getElementById('message').innerHTML = messages.correct;
+    } else if ((userSelect != rightAnswerIndex) && (answered == true)) {
+        incorrectAnswer++;
+        document.getElementById('message').innerHTML = messages.incorrect;
+        document.getElementById('correctedAnswer').innerHTML = 'The correct answer was: ' + rightAnswerText;
+    } else {
+        unanswered++;
+        document.getElementById('message').innerHTML = messages.endTime;
+        document.getElementById('correctedAnswer').innerHTML = 'The correct answer was: ' + rightAnswerText;
+        answered = true;
+    }
+
+    if (currentQuestion == (triviaQuestions.length - 1)) {
+        setTimeout(scoreboard, 5000)
+    } else {
+        currentQuestion++;
+        setTimeout(newQuestion, 5000);
+    }
+}
+
+function scoreboard() {
+    document.getElementById('timeLeft').innerHTML = '';
+    document.getElementById('message').innerHTML = '';
+    document.getElementById('correctedAnswer').innerHTML = '';
+    document.getElementById('finalMessage').innerHTML = messages.finished;
+    document.getElementById('correctAnswers').innerHTML = "Correct Answers: " + correctAnswer;
+    document.getElementById('incorrectAnswers').innerHTML = "Incorrect Answers: " + incorrectAnswer;
+    document.getElementById('unanswered').innerHTML = "Unanswered: " + unanswered;
+    document.getElementById("starOverBtn").className = "reset";
+    document.getElementById('startOverBtn').style.display = 'block';
+    document.getElementById('startOverBtn').innerHTML = 'Start Over?';
+}
